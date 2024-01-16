d407f6072d217d0265c16cef380129ab4407401d
Dominic Precious <itzdominion@gmail.com>
2024-01-16 23:17:36 +0100

    Add project files.

---
+++ Palindrome/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+namespace CheckPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("This program indicate whether a palindrome or not:");
+            string word = Console.ReadLine();
+            if(IsPalindrome(word))
+            {
+                Console.WriteLine($"{word} is a palindrome");
+            }else
+            {
+                Console.WriteLine($"{word} is not a palindrome");
+            }
+        }
+        public static bool IsPalindrome(string input)
+        {
+            char[] chars = input.ToCharArray();
+            Array.Reverse(chars);
+            var rev = new string(chars);
+            return input.Equals(rev, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
